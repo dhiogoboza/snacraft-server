1fef99030457c14dd9aaa4b8a128c6e2557f22ed
Ruben O. Chiavone <ruben.ochiavone@gmail.com>
2018-06-10 12:22:02 -0300

    Added ability to inform in game players count

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 
 from game import Game
 from client import Client
+from constants import Constants as Cts
 
 from flask import Flask, render_template
 from flask_sockets import Sockets
@@ -33,20 +34,33 @@ sockets = Sockets(app)
 def handle(ws):
     # new client connected
     client = Client(ws)
-     
+
+    ws.send(
+        message=",".join([Cts.MESSAGE_PLAYERS_SIZE, chr(len(game.clients))]),
+        binary=True)
+
+    # FIXME: find a way to check here if client is still present
+
     game.sendMap(client)
 
     # wait for snake's data
     data = ws.receive()
+
+    if data == None:
+        ws.close()
+        return
+
     data_split = data.split(",")
-    
+
     data = data_split[0]
     if (len(data) > 10):
         data = data[0:10]
-        
+
     client.setNickname(data)
-    
-    print(client.nickname, "connected")
+
+    nickname = client.nickname
+
+    print(nickname, "connected")
 
     game.addClient(client, int(data_split[1]))
     game.sendClientData(client)
@@ -62,3 +76,5 @@ def handle(ws):
 
     # terminate connection
     game.removeClient(client)
+
+    print(nickname, "disconnected")
